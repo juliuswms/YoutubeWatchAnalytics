d3f7dff04c70a0718ca89d8580d4db9e7ab59f14
eugene :) <71578259+juliuswms@users.noreply.github.com>
2023-03-24 13:15:25 +0100

    v.1
    Frist Commit.

---
+++ YoutubeWatchAnalytics/YoutubeWatchAnalytics/RawVideo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace YoutubeWatchAnalytics
+{
+    public class RawVideo
+    {
+        public string header { get; set; }
+        public string title { get; set; }
+        public string titleUrl { get; set; }
+        public string[] Subtitles { get; set; }
+        public string time { get; set; }
+        public string products { get; set; }
+        public string activityControls { get; set; }
+    }
+}
